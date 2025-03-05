178e5316f80857ceb02cc39cd97defed0067ee8a
Luke Sullivan <sully097@gmail.com>
2025-03-05 16:56:24 +0000

    Begin working on keyboard listeners

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const display = document.getElementById('display');
 
-const playDrumPad = pad => {
-    if (!pad.isPlaying) {    // Play audio node
-        pad.children[0].play();
-        console.log(pad.id);
-        console.log(pad.char);
-        display.innerText = pad.id;
+const playDrumPad = item => {
+    if (!item.isPlaying) {    // Play audio node
+        item.pad.children[0].play();
+        console.log(item.id);
+        // console.log(item.char);
+        console.log(toChar(item.char));
+        display.innerText = item.id;
     } else {}
+}
 
+const toChar = string => {
+    if (string.length > 1) return
+    else return string.toLowerCase().charCodeAt(0);
 }
 
 // Create array of .drum-pad objects and store play state
@@ -15,11 +20,11 @@ const drumArr = Array.from(document.getElementsByClassName('drum-pad')).map((dru
     pad: drumPad,
     isPlaying: false,
     id: drumPad.getAttribute('id'),
-    char: drumPad.textContent
+    char: drumPad.textContent.trim()
 }));
 
 drumArr.forEach((item) => {
-    item.pad.addEventListener("click", () => playDrumPad(item.pad));
+    item.pad.addEventListener("click", () => playDrumPad(item));
 });
 
 document.addEventListener("keypress", event => {
