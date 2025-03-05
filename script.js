1d236f3bfab6b5ab3ed844e21baee3be38c3289c
Luke Sullivan <sully097@gmail.com>
2025-03-05 15:41:40 +0000

    Each noise added to drum arr and working

--- script.js
+++ script.js
@@ -9,10 +9,11 @@ const playAudioChild = element => {
 const drumArr = Array.from(document.getElementsByClassName('drum-pad'));
 
 drumArr.forEach((item, index) => {
-    setTimeout(() => {
-        playAudioChild(item);
-        console.log(item);
-    }, index * 500); // Increase delay based on index
+    // setTimeout(() => {
+    //     playAudioChild(item);
+    //     console.log(item);
+    // }, index * 500); // Increase delay based on index
+    item.addEventListener("click", () => playAudioChild(item));
 });
 
 document.addEventListener("keypress", event => {
@@ -24,5 +25,5 @@ document.addEventListener("keypress", event => {
 });
 
 const clickDrumPad = () => {
-
+    
 }
