88ae6637c7dfd034c258264ad880319e1b96d682
Luke Sullivan <sully097@gmail.com>
2025-03-06 09:14:44 +0000

    Update script.js formatting

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const display = document.getElementById('display');
 let currentAudio = null;
 
+// Create array of .drum-pad objects and store id and keycode characters
+const drumArr = Array.from(document.getElementsByClassName('drum-pad')).map((drumPad) => ({
+    pad: drumPad,
+    id: drumPad.getAttribute('id'),
+    char: drumPad.textContent.trim().toLowerCase()
+}));
+
 // Hit drum, play sound
 const playDrumPad = item => {
-    const audioChild = item.pad.children[0];
-
     // If there's audio currently playing, stop it
     if (currentAudio && !currentAudio.paused) {
         currentAudio.pause();
@@ -12,6 +17,7 @@ const playDrumPad = item => {
     }
 
     // Play sound and update display
+    const audioChild = item.pad.children[0];
     audioChild.play();
     currentAudio = audioChild;
     display.textContent = item.id;
@@ -23,17 +29,6 @@ const playDrumPad = item => {
     }, 100);
 };
 
-// Create array of .drum-pad objects and store id and keycode characters
-const drumArr = Array.from(document.getElementsByClassName('drum-pad')).map((drumPad) => ({
-    pad: drumPad,
-    id: drumPad.getAttribute('id'),
-    char: drumPad.textContent.trim().toLowerCase()
-}));
-
-drumArr.forEach((item) => {
-    item.pad.addEventListener("click", () => playDrumPad(item));
-});
-
 // Play drum pad if correct key is pressed
 document.addEventListener("keydown", event => {
     const charString = event.key.toLowerCase();
@@ -42,4 +37,8 @@ document.addEventListener("keydown", event => {
     if (keyPress) {
         playDrumPad(keyPress);
     }
+});
+
+drumArr.forEach((item) => {
+    item.pad.addEventListener("click", () => playDrumPad(item));
 });
